62b329b17640d20334ccfc8500076160391f221e
jester <jaimebuddy98@gmail.com>
2022-01-15 16:08:38 -0600

    generated html based on user input

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage();
 
 // fs.writeFile('./index.html', pageHTML, err => {
 //   if (err) throw err;
@@ -143,6 +143,12 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
 
